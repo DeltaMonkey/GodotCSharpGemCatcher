41ce61be28ffdd7dca341c8c4f5cda1edf9ca47e
DeltaMonkey <engincanozkan@outlook.com>
2025-01-20 15:46:34 +0300

    20_Referancing Nodes & load symbols & supress module load logs

--- Scenes/Game/Game.cs
+++ Scenes/Game/Game.cs
@@ -3,8 +3,15 @@ using System;
 
 public partial class Game : Node2D
 {
+    [Export] private Gem _gem;
+    //[Export] private NodePath _gemPath; // Game/Gem
+    //private Gem _gem;
+
     public override void _Ready()
     {
+        //Gem gem = GetNode<Gem>("Gem");
+        //_gem = GetNode<Gem>(_gemPath);
+        _gem.OnScored += OnScored;
     }
 
     private void OnScored()
